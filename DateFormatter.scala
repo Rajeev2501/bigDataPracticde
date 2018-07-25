845ff6c365867dbbc24644fe6efc87170e5482d7
Rajeev2501 <41697281+Rajeev2501@users.noreply.github.com>
2018-07-25 22:49:03 +0530

    Update DateFormatter.scala

--- DateFormatter.scala
+++ DateFormatter.scala
@@ -6,14 +6,24 @@ import java.util.Calendar
 import java.util.Locale
 
 object DateFormatter {
+  //expected input is in format yyyyMMdd
   
   def schdDateFormatter(scheduled_date: String): String = {
     val dateFormat = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH);
     val schdDayFormat = dateFormat.parse(scheduled_date);
+    //instantiating Calendar object to be used further
     var cal = Calendar.getInstance()
+    
+    //setting the supplied time to the cal object 
     cal.setTime(schdDayFormat)
+    
+    //Reducing 1 day from the scheduled date
     cal.add(Calendar.DATE, -1);
     var todate1 = cal.getTime();
     dateFormat.format(todate1).toString()
+    
+    /*
+    *input = 20160101 output = 20151231
+    */
   }
 }
